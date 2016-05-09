3dbfb512645de7cd76d403dda83f9d58a67ea332
Jacob Dodson <foodflare@yahoo.com>
2016-05-09 15:37:27 -0700

    The Rapture :: I changed nothing

---
+++ Bounty Hunter Simulator 2016/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int playerNum;
+
+    public float speed;
+
+    private float horizMove, horizFace, vertMove, vertFace;
+    private Rigidbody rb;
+    private Transform tf;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        tf = GetComponent<Transform>();
+        if (playerNum == 0)
+            playerNum = 1;
+    }
+
+    void Update()
+    {
+        horizMove = Input.GetAxis("moveHoriz_P" + playerNum);
+        horizFace = Input.GetAxis("faceHoriz_P" + playerNum);
+        vertMove = Input.GetAxis("moveVert_P" + playerNum);
+        vertFace = Input.GetAxis("faceVert_P" + playerNum);
+    }
+
+    void FixedUpdate()
+    {
+        tf.forward = new Vector3(horizFace, 0.0f, vertFace);
+        rb.velocity = new Vector3(horizMove, 0.0f, vertMove) * speed;
+    }
+
+    public void setNum(int id)
+    {
+        playerNum = id;
+    }
+}
