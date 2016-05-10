a7ebe61c4131af03f43ed7ed3f302420c70cad01
Ben Mecklenburg <bmecklenburg16@gmail.com>
2016-05-10 11:36:59 -0700

    Made Melee enemy 1, need to account for multiple players

--- Bounty Hunter Simulator 2016/Assets/Scripts/EnemyType1.cs
+++ Bounty Hunter Simulator 2016/Assets/Scripts/EnemyType1.cs
@@ -4,14 +4,16 @@ using System.Collections;
 public class EnemyType1 : MonoBehaviour
 {
     //movement finite state machine
-    enum ControlState {STAND, WALK };
+    enum ControlState {STAND, WALK, RETURN };
     private ControlState movementState;
 
     //Agent info
     private Vector3 startLoc, targetLoc;
     private Rigidbody rb;
     private Transform tf;
-    public GameObject player;
+    public GameObject[] players;
+    public NavMeshAgent nav;
+    public float maxRange;
     public float moveSpeed;
 
 	// Use this for initialization
@@ -19,27 +21,59 @@ public class EnemyType1 : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         tf = GetComponent<Transform>();
+        nav = GetComponent<NavMeshAgent>();
         movementState = ControlState.STAND;
         tf.forward = Vector3.zero;
-       
+        startLoc = tf.position;
+
+        if (players == null)
+            players = GameObject.FindGameObjectsWithTag("Player");
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        MovementBehavior();
+        //TODO: make logic to see which player is closer, chase closer player
+        if(players != null)
+        {
+            MovementBehavior();
+        }
     }
     void MovementBehavior()
     {
         switch(movementState)
         {
             case ControlState.STAND:
-                startLoc = tf.position;
+                if (Vector3.Distance(startLoc, players[0].transform.position) < maxRange)
+                {
+                    movementState = ControlState.WALK;
+                }
+                if (Vector3.Distance(tf.position, startLoc) > 1.0f)
+                    movementState = ControlState.RETURN;
                 break;
 
             case ControlState.WALK:
-                targetLoc = player.transform.position;
-//                rb.velocity = Vector3.Normalize(new Vector3(, 0, )) * moveSpeed;
+                //nav.destination = player.transform.position;
+                tf.LookAt(players[0].transform);
+                tf.position += tf.forward * moveSpeed * Time.deltaTime;
+                if(Vector3.Distance(startLoc, players[0].transform.position) > maxRange)
+                {
+                    movementState = ControlState.STAND;
+                }
+                break;
+
+            case ControlState.RETURN:
+                //nav.destination = startLoc;
+                tf.LookAt(startLoc);
+                tf.position += tf.forward * moveSpeed * Time.deltaTime;
+                if(Vector3.Distance(startLoc, tf.position) < 0.25f)
+                {
+                    movementState = ControlState.STAND;
+                }
+                if (Vector3.Distance(startLoc, players[0].transform.position) < maxRange)
+                {
+                    movementState = ControlState.WALK;
+                }
                 break;
         }
     }
