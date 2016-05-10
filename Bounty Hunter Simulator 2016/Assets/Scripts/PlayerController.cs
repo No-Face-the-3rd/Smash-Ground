d2674cc7b2a5f2dcbe0f1d78a1f1b88ec46861f4
Jacob Dodson <foodflare@yahoo.com>
2016-05-09 17:43:30 -0700

    Controller inputs and no default facing when let go of face control

--- Bounty Hunter Simulator 2016/Assets/Scripts/PlayerController.cs
+++ Bounty Hunter Simulator 2016/Assets/Scripts/PlayerController.cs
@@ -30,7 +30,8 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        tf.forward = new Vector3(horizFace, 0.0f, vertFace);
+        if(Mathf.Abs(horizFace) > Mathf.Epsilon || Mathf.Abs(vertFace) > Mathf.Epsilon)
+            tf.forward = new Vector3(horizFace, 0.0f, vertFace);
         rb.velocity = new Vector3(horizMove, 0.0f, vertMove) * speed;
     }
 
