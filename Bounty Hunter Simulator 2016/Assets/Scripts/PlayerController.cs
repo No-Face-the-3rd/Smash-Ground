7f4c5404a8711addcd80f405fbc575bca3002c1e
Jacob Dodson <foodflare@yahoo.com>
2016-05-10 17:28:46 -0700

    Implemented some code for character selection

--- Bounty Hunter Simulator 2016/Assets/Scripts/PlayerController.cs
+++ Bounty Hunter Simulator 2016/Assets/Scripts/PlayerController.cs
@@ -7,36 +7,21 @@ public class PlayerController : MonoBehaviour
     public int playerNum;
 
     public float speed;
+    public float panSpeed;
 
     private float horizMove, horizFace, vertMove, vertFace;
     private Rigidbody rb;
     private Transform tf;
+    private Vector3 csTarg;
     public Camera cs;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         tf = GetComponent<Transform>();
-        if (playerNum == 0)
-            playerNum = 1;
-
-        GameObject orig = GameObject.FindGameObjectWithTag("MainCamera");
-        cs = ((GameObject)Instantiate(orig, new Vector3(0.0f,4.0f,4.0f), Quaternion.Euler(0.0f,180.0f,0.0f))).GetComponent<Camera>();
-        cs.name = "Player " + playerNum + " Camera";
-        cs.tag = "CharSelectCam";
-        cs.backgroundColor = Color.black;
-        cs.depth = playerNum;
-        cs.cullingMask = 1 << 12;
-        cs.orthographic = false;
-
-        cs.pixelRect = new Rect(cs.pixelRect.xMin, cs.pixelRect.yMin, cs.pixelRect.width * 0.15f, cs.pixelRect.height * 0.15f);
-        ((AudioListener)cs.GetComponent(typeof(AudioListener))).enabled = false;
-        ((GUILayer)cs.GetComponent(typeof(GUILayer))).enabled = false;
-        RenderTexture texture = new RenderTexture(512, 512, 16);
-        cs.targetTexture = texture;
-        cs.fieldOfView = 20.0f;
-
-
+        setNum(1);
+//            if (playerNum == 0)
+//                playerNum = 1;
     }
 
     void Update()
@@ -49,13 +34,61 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(Mathf.Abs(horizFace) > Mathf.Epsilon || Mathf.Abs(vertFace) > Mathf.Epsilon)
-            tf.forward = new Vector3(horizFace, 0.0f, vertFace);
-        rb.velocity = Vector3.Normalize(new Vector3(horizMove, 0.0f, vertMove)) * speed;
+        if (tf.childCount > 0)
+            movePlayer();
+        else
+            moveCam();
     }
 
     public void setNum(int id)
     {
         playerNum = id;
+        createCam();
+    }
+
+    void createCam()
+    {
+        csTarg = new Vector3(0.0f, 3.5f, 4.0f);
+        GameObject orig = GameObject.FindGameObjectWithTag("MainCamera");
+        cs = ((GameObject)Instantiate(orig, csTarg, Quaternion.Euler(0.0f, 180.0f, 0.0f))).GetComponent<Camera>();
+        cs.name = "Player " + playerNum + " Camera";
+        cs.tag = "CharSelectCam";
+        cs.backgroundColor = Color.black;
+        cs.depth = playerNum;
+        cs.cullingMask = 1 << (11 + playerNum);
+        cs.orthographic = false;
+
+        cs.pixelRect = new Rect((0.01f + (float)(playerNum - 1) * 0.78f) * cs.pixelRect.width, 0.7f * cs.pixelRect.height, 0.2f * cs.pixelRect.width, 0.26f * cs.pixelRect.height);
+        ((AudioListener)cs.GetComponent(typeof(AudioListener))).enabled = false;
+        ((GUILayer)cs.GetComponent(typeof(GUILayer))).enabled = false;
+
+        cs.fieldOfView = 20.0f;
+        csTarg = new Vector3(0.0f, 3.5f, 0.0f);
+
+    }
+
+    void moveCam()
+    {
+        Vector3 csPos = cs.GetComponent<Transform>().position;
+        float dist = Vector3.Distance(csPos, csTarg);
+        cs.GetComponent<Transform>().Rotate(new Vector3(-vertMove , horizMove , 0.0f) * panSpeed);
+        cs.GetComponent<Transform>().position = csTarg + -(dist * cs.GetComponent<Transform>().forward);
+        cs.GetComponent<Transform>().rotation = Quaternion.Euler(new Vector3(cs.GetComponent<Transform>().rotation.eulerAngles.x, cs.GetComponent<Transform>().rotation.eulerAngles.y, 0.0f));
+
+    }
+    void movePlayer()
+    {
+        //reset cam when playing
+        if (cs.GetComponent<Transform>().rotation != Quaternion.Euler(new Vector3(0.0f, 180.0f, 0.0f)))
+        {
+            Vector3 csPos = cs.GetComponent<Transform>().position;
+            float dist = Vector3.Distance(csPos, csTarg);
+            cs.GetComponent<Transform>().rotation = Quaternion.Euler(new Vector3(0.0f, 180.0f, 0.0f));
+            cs.GetComponent<Transform>().position = csTarg + -(dist * cs.GetComponent<Transform>().forward);
+        }
+        if (Mathf.Abs(horizFace) > Mathf.Epsilon || Mathf.Abs(vertFace) > Mathf.Epsilon)
+            tf.forward = new Vector3(horizFace, 0.0f, vertFace);
+
+        rb.velocity = Vector3.Normalize(new Vector3(horizMove, 0.0f, vertMove)) * speed;
     }
 }
