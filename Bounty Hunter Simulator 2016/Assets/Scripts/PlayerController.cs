8b7e722f6c54dd749d7f8c71b22f5b03d6e1e6f2
Jacob Dodson <foodflare@yahoo.com>
2016-05-11 13:02:06 -0700

    Implemented flexible system to create characters

--- Bounty Hunter Simulator 2016/Assets/Scripts/PlayerController.cs
+++ Bounty Hunter Simulator 2016/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public class character : MonoBehaviour
+{
+    public float primaryDelay, secondaryDelay, dodgeDelay, globalDelay, dodgeSpd, dodgeTime;
+    private float nextPrimary, nextSecondary, nextDodge;
+    public GameObject primaryPref, secondaryPref;
+    private Transform ptf, tf;
+    private Rigidbody prb;
+    private bool dodging;
+    private Vector3 dodgeDir;
+    void Start()
+    {
+        nextPrimary = nextSecondary = nextDodge = 0.0f;
+        tf = GetComponent<Transform>();
+        ptf = tf.parent;
+        prb = ptf.GetComponent<Rigidbody>();
+        dodging = false;
+        dodgeDir = new Vector3(0.0f,1.0f,0.0f);
+    }
+    public virtual void processInput(float primary, float secondary, float dodge)
+    {
+        if (Mathf.Abs(primary) > 0.0f && Time.time > nextPrimary)
+        {
+            nextPrimary = Time.time + primaryDelay;
+            nextSecondary = Time.time + globalDelay;
+            nextDodge = Time.time + globalDelay;
+            doPrimary();
+        }
+        if (Mathf.Abs(secondary) > 0.0f && Time.time > nextSecondary)
+        {
+            nextPrimary = Time.time + globalDelay;
+            nextSecondary = Time.time + secondaryDelay;
+            nextDodge = Time.time + globalDelay;
+            doSecondary();
+        }
+        if (Mathf.Abs(dodge) > 0.0f && Time.time > nextDodge)
+        {
+            if(!(nextPrimary > Time.time + globalDelay + dodgeTime))
+                nextPrimary = Time.time + dodgeTime + globalDelay;
+            nextSecondary = Time.time + dodgeTime + globalDelay;
+            nextDodge = Time.time + dodgeTime + dodgeDelay;
+            dodging = true;
+        }
+        if (dodging && nextDodge - dodgeDelay - Time.time > 0.0f)
+            doDodge();
+        else
+        {
+            ptf.gameObject.layer = 8;
+            dodgeDir = new Vector3(0.0f, 1.0f, 0.0f);
+        }
+    }
+
+    public virtual void doPrimary()
+    {
+        Instantiate(primaryPref, tf.position + tf.forward * 0.5f + new Vector3(0.0f, 3.5f, 0.0f), Quaternion.Euler(tf.forward));
+    }
+
+    public virtual void doSecondary()
+    {
+        Instantiate(secondaryPref, tf.position + tf.forward * 0.5f + new Vector3(0.0f, 3.5f, 0.0f), Quaternion.Euler(tf.forward));
+    }
+
+    public virtual void doDodge()
+    {
+        if (dodgeDir == new Vector3(0.0f, 1.0f, 0.0f))
+            dodgeDir = Vector3.Normalize(prb.velocity);
+        Vector3 newVel = ((dodgeDir * dodgeSpd) + prb.velocity);
+        prb.velocity = newVel;
+        ptf.gameObject.layer = 15;
+    }
+}
+
+
 public class PlayerController : MonoBehaviour
 {
 
@@ -10,16 +82,20 @@ public class PlayerController : MonoBehaviour
     public float panSpeed;
 
     private float horizMove, horizFace, vertMove, vertFace;
+    private float primaryIn, secondaryIn, dodgeIn;
     private Rigidbody rb;
     private Transform tf;
     private Vector3 csTarg;
     public Camera cs;
 
+    private character child;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         tf = GetComponent<Transform>();
-        setNum(1);
+        setNum(playerNum);
+        child = tf.GetChild(0).GetComponent<character>();
 //            if (playerNum == 0)
 //                playerNum = 1;
     }
@@ -30,12 +106,18 @@ public class PlayerController : MonoBehaviour
         horizFace = Input.GetAxis("faceHoriz_P" + playerNum);
         vertMove = Input.GetAxis("moveVert_P" + playerNum);
         vertFace = Input.GetAxis("faceVert_P" + playerNum);
+        primaryIn = Input.GetAxis("primaryAttack_P" + playerNum);
+        secondaryIn = Input.GetAxis("secondaryAttack_P" + playerNum);
+        dodgeIn = Input.GetAxis("dodge_P" + playerNum);
     }
 
     void FixedUpdate()
     {
         if (tf.childCount > 0)
+        {
             movePlayer();
+            child.processInput(primaryIn, secondaryIn, dodgeIn);
+        }
         else
             moveCam();
     }
