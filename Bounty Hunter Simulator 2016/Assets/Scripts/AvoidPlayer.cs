54a4d24ffa3d2042f7d35c8d3f7c6ef4ea05baa1
RaskoSmash <bmecklenburg16@gmail.com>
2016-05-17 17:49:55 -0700

    Added melee

---
+++ Bounty Hunter Simulator 2016/Assets/Scripts/AvoidPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private GameObject[] players;
+    private Transform tf;
+    
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        tf = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
