4803625af0096571668d91fb33f75d223002a25b
No-Face-the-3rd <foodflare@yahoo.com>
2016-05-24 17:50:06 -0700

    worked on pinecone projectile and added more assets

---
+++ Bounty Hunter Simulator 2016/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+    public int owner;
+    public int damage;
+    public float speed;
+
+
+
+	void Start ()
+    {
+	
+	}
+
+	void Update ()
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+
+    }
+}
